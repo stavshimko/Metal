1344275882540e642499f042b23516db83d702dc
stavshimko <stavshimko@gmail.com>
2020-06-15 15:23:14 +0300

    click ME

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World! It's TZVAIG 2
-    </div>
-  );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            age: ""
+        }
+    }
+
+    ClickMe = () => {
+        this.setState({
+            name: "Stav"
+         });
+    }
+
+    render () {
+        // console.log(this.state.name)
+        return (
+            <div className="App">
+                <button onClick={this.ClickMe}>Click Me</button>
+                Hello World! It's TZVAIG 2 <span className="name"> {this.state.name} </span>
+            </div>
+        );
+    }
 }
 
-export default App;
